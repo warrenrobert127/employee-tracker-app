7661ca28341a7ea38f30c1484b918f1bcaad8389
Robert Warren <warrenrobert127@gmail.com>
2022-02-22 17:12:43 -0600

    choices

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 //fix these routes
 
-const apiRoutes = require("./routes/apiRoutes.js");
-const htmlRoutes = require("./routes/htmlRoutes.js");
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+   console.log(`API server now on port ${PORT}!`);
 });
