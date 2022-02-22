710e682871213130fddfab5f41ea91298c57e08a
Robert Warren <warrenrobert127@gmail.com>
2022-02-22 17:02:54 -0600

    setup

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments(
+id INT AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL(8,2),
+department_id INT,
+FOREIGN KEY(department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees(
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+FOREIGN KEY(role_id) REFERENCES roles(id),
+FOREIGN KEY(manager_id) REFERENCES employees(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO departments(department_name)
+VALUES ('Management'),
+('Sales'),
+('Warehouse'),
+('Human Resources'),
+('Quality Control');
+
+INSERT INTO roles(title)
+VALUES('Regional Manager'),
+('Sales Rep'),
+('HR Rep');
+
+INSERT INTO employees(first_name, last_name)
+VALUES('Kyle', 'Schwarber'),
+('Calvin', 'Brodous'),
+('Curtis', 'Jackson');
